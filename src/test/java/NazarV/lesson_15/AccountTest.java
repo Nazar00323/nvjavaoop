3037b7122d8e1af7fa6f5c2e41653cdeab256903
NAzarMh <nazar.myhailiv@gmail.com>
2017-01-16 16:34:19 +0200

    Lesson 15 HomeWork Test

---
+++ src/test/java/NazarV/lesson_15/AccountTest.java
@@ -0,0 +1,39 @@
+package NazarV.lesson_15;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AccountTest {
+    @Test
+    public void numberTest(){
+        Account ac = new Account();
+        int number  = 8427456878965623;
+        ac.setNumber(number);
+        assertEquals(number,ac.getNumber());
+    }
+    @Test
+    public void balanceTest(){
+        Account ac = new Account();
+        int balance = 5000;
+        ac.setBalance(balance);
+        assertEquals(balance,ac.getBalance());
+    }
+    @Test
+    public void getMoney(){
+        Account ac = new Account();
+        int balance = 5000;
+        int money = 200;
+        ac.setBalance(balance);
+        ac.getMoney(money);
+        assertEquals(balance-money,ac.getBalance());
+    }
+    @Test
+    public void putMoney(){
+        Account ac = new Account();
+        int balance = 5000;
+        int money = 200;
+        ac.setBalance(balance);
+        ac.putMoney(money);
+        assertEquals(balance+money,ac.getBalance());
+    }
+}
