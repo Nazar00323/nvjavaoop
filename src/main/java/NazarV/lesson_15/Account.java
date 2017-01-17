a8e2614d174f0ab6437755b3416fdae2a32d3941
NazarV <naz.00323@gmail.com>
2017-01-17 22:41:31 +0500

    Finish  task 1501

--- src/main/java/NazarV/lesson_15/Account.java
+++ src/main/java/NazarV/lesson_15/Account.java
@@ -1,4 +1,33 @@
 package NazarV.lesson_15;
+
 /*Task 1501 1502*/
 public class Account {
+    private String number;
+    private int balance;
+    private int money;
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public void putMoney(int money) {
+    }
+
+    public int getMoney(int money) {
+        int sum = money;
+
+        return sum;
+    }
 }
