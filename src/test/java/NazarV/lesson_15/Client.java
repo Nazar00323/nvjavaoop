284a5d7792ecfba4853b8fce4341ab44bcfd76b6
NazarV <naz.00323@gmail.com>
2017-01-21 17:10:57 +0500

    Finish  task 1601

--- src/test/java/NazarV/lesson_15/Client.java
+++ src/test/java/NazarV/lesson_15/Client.java
@@ -1,7 +1,31 @@
 package NazarV.lesson_15;
 
-/**
- * Created by NAZZIK on 21.01.2017.
- */
 public class Client {
+    private String Name;
+    private String Surname;
+    private String Phone;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getSurname() {
+        return Surname;
+    }
+
+    public void setSurname(String surname) {
+        Surname = surname;
+    }
+
+    public String getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(String phone) {
+        Phone = phone;
+    }
 }
