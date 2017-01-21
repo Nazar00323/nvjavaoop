133c561682395992a274fd41b13ea32fd45e4cd7
NAzarMh <nazar.myhailiv@gmail.com>
2017-01-21 09:30:41 +0200

    add new Task 1602, 1603, 1604

---
+++ src/test/java/NazarV/lesson_15/ClientTest.java
@@ -0,0 +1,37 @@
+package NazarV.lesson_15;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ClientTest {
+    @Test
+    // Task 1602
+    public void nameTest() {
+        Client cl = new Client();
+        String tempName = "Nazar";
+        cl.setName(tempName);
+        assertEquals(tempName, cl.getName());
+    }
+
+    // End Task 1602
+    @Test
+    // Task 1603
+    public void surnameTest() {
+        Client cl = new Client();
+        String tempSurname = "Voychack";
+        cl.setSurname(tempSurname);
+        assertEquals(tempSurname, cl.getSurname());
+    }
+
+    // End Task 1603
+    @Test
+    // Task 1604
+    public void phoneTest() {
+        Client cl = new Client();
+        String tempPhone = "0971073443";
+        cl.setPhone(tempPhone);
+        assertEquals(tempPhone, cl.getPhone());
+    }
+    // End Task 1604
+}
