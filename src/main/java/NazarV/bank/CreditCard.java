8672410ecf9196ba81ab490164086d15c3edcbe1
NazarV <naz.00323@gmail.com>
2017-02-26 13:46:23 -0100

    Finish Task 2701

--- src/main/java/NazarV/bank/CreditCard.java
+++ src/main/java/NazarV/bank/CreditCard.java
@@ -4,6 +4,43 @@ package NazarV.bank;
  * Created by NAZZIK on 25.02.2017.
  */
 public class CreditCard extends Card {
+    private  int validity;
+    private  int validation;
+
+    public CreditCard() {
+        super();
+        this.validity = -1;
+        this.validation = -1;
+    }
+
+    public CreditCard(int validity, int validation) {
+        this.validity = validity;
+        this.validation = validation;
+    }
+
+    public int getValidity() {
+        return validity;
+    }
+
+    public void setValidity(int validity) {
+        this.validity = validity;
+    }
+
+    public int getValidation() {
+        return validation;
+    }
+
+    public void setValidation(int validation) {
+        this.validation = validation;
+    }
+    public String toString() {
+
+        return "CreditCard [validity = " + this.getValidity()
+                + " validation = " + this.getValidation()
+                + " month = " + this.getMonth()
+                + " year = " + this.getYear()
+                + " cvc = " + this.getCvc() + "]";
+    }
 
 
 }
